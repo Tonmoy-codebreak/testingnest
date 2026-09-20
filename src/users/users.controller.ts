70a9042f7a2cdce98463b3fbe0d31e02b896526d
Abrar Hossain <tonmoyallalong@gmail.com>
2026-09-20 17:22:01 +0600

    added role guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,10 +6,15 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard.js';
+import { RolesGuard } from '../auth/guards/roles.guard.js';
+import { Roles } from '../auth/decorators/roles.decorator.js';
+import { Role } from '../generated/client/enums.js';
 
 @Controller('users')
 export class UsersController {
@@ -23,6 +28,13 @@ export class UsersController {
   @Get()
   findAll() {
     return this.usersService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.SUPER_ADMIN)
+  @Get('admin-test')
+  adminOnly() {
+    return { message: 'You are an admin' };
   }
 
   @Get(':id')
